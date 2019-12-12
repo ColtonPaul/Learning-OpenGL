b954578e9e671fa841ace602e02e6e1184080b08
Colton Paul <colton.paul@flow3d.com>
2019-12-12 10:45:55 -0700

    Section 2.19 - Input with GLFW -Added members to the Window class to store key presses and mouse movements. -Added callbacks to the Window class to handle mouse movements and key presses.

--- OpenGLCourseApp/OpenGLCourseApp/Window.cpp
+++ OpenGLCourseApp/OpenGLCourseApp/Window.cpp
@@ -6,12 +6,66 @@ Window::Window()
 {
     width = 800;
     height = 600;
+    this->initializeKeys();
 }
 
 Window::Window(GLint windowWidth, GLint windowHeight)
 {
     width = windowWidth;
     height = windowHeight;
+    this->initializeKeys();
+}
+
+void Window::initializeKeys()
+{
+    for (size_t i = 0; i < 1024; i++)
+    {
+        keys[i] = 0;
+    }
+}
+
+void Window::handleKeys(GLFWwindow * window, int key, int code, int action, int mode)
+{
+    //This gets the window that we set in Window::initialize(), which lets us work with the window in a static 
+    Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+    if (key >= 0 && key < 1024)
+    {
+        if (action == GLFW_PRESS)
+        {
+            theWindow->keys[key] = true;
+        }
+        else if (action == GLFW_RELEASE)
+        {
+            theWindow->keys[key] = false;
+        }
+    }
+}
+
+void Window::handleMouse(GLFWwindow * window, double xPos, double yPos)
+{
+    Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+    if (theWindow->mouseFirstMoved) //if it's the very first movement
+    {
+        theWindow->lastX = xPos;
+        theWindow->lastY = yPos;
+        theWindow->mouseFirstMoved = false;
+    }
+    theWindow->xChange = xPos - theWindow->lastX;
+    theWindow->yChange = theWindow->lastY - yPos;
+
+    theWindow->lastX = xPos;
+    theWindow->lastY = yPos;
+}
+
+void Window::createCallbacks()
+{
+    glfwSetKeyCallback(mainWindow, handleKeys); //Like a signal/slot between a key press on the main window and the callback handleKeys;
+    glfwSetCursorPosCallback(mainWindow, handleMouse);
 }
 
 int Window::initialize()
@@ -51,6 +105,11 @@ int Window::initialize()
     //I.e. We want to tie the openGL context to this window
     glfwMakeContextCurrent(mainWindow);
 
+    //Handle key and mouse input
+    this->createCallbacks();
+
+    glfwSetInputMode(mainWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED); //Doesn't display the cursor to the screen
+
     //Allow modern extension features
     glewExperimental = GL_TRUE;
 
@@ -66,6 +125,22 @@ int Window::initialize()
 
     //Set up viewport size (Sets up what part we're drawing to on our window)
     glViewport(0, 0, bufferWidth, bufferHeight);
+
+    glfwSetWindowUserPointer(mainWindow, this);
+}
+
+GLfloat Window::getXChange()
+{
+    GLfloat theChange = xChange;
+    xChange = 0.0f;
+    return theChange;
+}
+
+GLfloat Window::getYChange()
+{
+    GLfloat theChange = yChange;
+    yChange = 0.0f;
+    return theChange;
 }
 
 Window::~Window()
--- OpenGLCourseApp/OpenGLCourseApp/Window.h
+++ OpenGLCourseApp/OpenGLCourseApp/Window.h
@@ -12,6 +12,11 @@ public:
     int initialize();
     GLint getBufferWidth() { return bufferWidth; }
     GLint getBufferHeight() { return bufferHeight; }
+
+    bool* getKeys() { return keys; } //we could instead make the API request information on an individual key
+    GLfloat getXChange();
+    GLfloat getYChange();
+
     bool shouldClose() { return glfwWindowShouldClose(mainWindow); }
     void swapBuffers() { glfwSwapBuffers(mainWindow); };
     ~Window();
@@ -20,5 +25,21 @@ private:
 
     GLint width, height;
     GLint bufferWidth, bufferHeight;
+
+    bool keys[1024]; //covers the range of keyboard characters to store key presses
+
+    GLfloat lastX;
+    GLfloat lastY;
+    GLfloat xChange;
+    GLfloat yChange;
+    bool mouseFirstMoved; //takes into account the initial movement, so the screen doesn't jump when the mouse first moves into the screen
+
+    void initializeKeys();
+
+    //We have to put all five values in because it's expected for the API.
+    //we need a static function to use it as a callback
+    static void handleKeys(GLFWwindow* window, int key, int code, int action, int mode);
+    static void handleMouse(GLFWwindow* window, double xPos, double yPos);
+    void createCallbacks();
 };
 
